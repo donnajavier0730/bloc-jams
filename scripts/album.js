d79da922fad308dc8e72ee0634d32d934ae8834d
Primadonna Javier <primadonnajavier@yahoo.com>
2017-03-26 15:31:40 -0400

    assignment-11-DOM-Scripting-AlbumView:completed

--- scripts/album.js
+++ scripts/album.js
@@ -10,7 +10,7 @@ var albumPicasso = {
         { title: 'Green', duration: '3:14'},
         { title: 'Red', duration: '5:01'},
         { title: 'Pink', duration: '3:21'},
-        { title: 'Magenta', duration: '2:15'},
+        { title: 'Magenta', duration: '2:15'}
     ]
 };
 
@@ -26,47 +26,75 @@ var albumMarconi = {
         { title: 'Ring, ring, ring', duration: '5:01'},
         { title: 'Fits in your pocket', duration: '3:21'},
         { title: 'Can you hear me now?', duration: '3:14'},
-        { title: 'Wrong phone number', duration: '2:15'},
+        { title: 'Wrong phone number', duration: '2:15'}
     ]
 };
 
-var createSongRow = function(songNumber, songName, songLength){
-    var template = '<tr class="album-view-song-item">'
-    + '<td class="song-item-number">' + songNumber + '</td>'
-    + '<td class="song-item-title">' + songName + '</td>'
-    + '<td class="song-item-duration">' + songLength + '</td>'
-    + '</tr>'
-    ;
-    
-    return template;
+//third album of my choice
+var albumMaroon5 = {
+    title: 'OverExposed',
+    artist: 'Maroon5',
+    label: 'Deluxed',
+    year: '2012',
+    albumArtUrl: 'assets/images/album_covers/maroon5.jpg',
+    songs: [
+        { title: 'One More Night', duration: '3.05'},
+        { title: 'Payphone', duration: '3.05'},
+        { title: 'Daylight', duration: '4.05'},
+        { title: 'Lucky Strike', duration: '5.05'},
+        { title: 'The Man Who Never Lied', duration: '6.05'}
+    ]
 };
 
-var setCurrentAlbum = function(album){
-    //1
+var createSongRow = function(songNumber, songName, songLength) {
+     var template =
+        '<tr class="album-view-song-item">'
+      + '  <td class="song-item-number">' + songNumber + '</td>'
+      + '  <td class="song-item-title">' + songName + '</td>'
+      + '  <td class="song-item-duration">' + songLength + '</td>'
+      + '</tr>'
+      ;
+ 
+     return template;
+ };
+
+ //1
     var albumTitle = document.getElementsByClassName('album-view-title')[0];
     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-    var albumReleaseInfo = dosument.getElementsByClassName('album-view-release-info')[0];
+    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
     var albumImage = document.getElementsByClassName('album-cover-art')[0];
     var albumSongList = document.getElementsByClassName('album-view-song-list')[0]
     
-    //2
-    albumTitle.firstChild.nodeValue = album.title;
-    albumArtist.firstChild.nodeValue = album.artist;
-    albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
-    albumImage.setAttribute('src', album.albumArtUrl);
-    
-    //3
-    albumSongList.innerHTML = '';
-    
-    //4
-    for (var i = 0; i<album.songs.length; i++){
-        albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
-    }
-};
 
-window.onload = function(){
-    setCurrentAlbum(albumPicasso);
-};
+var setCurrentAlbum = function(album) {
+     albumTitle.firstChild.nodeValue = album.title;
+     albumArtist.firstChild.nodeValue = album.artist;
+     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
+     albumImage.setAttribute('src', album.albumArtUrl);
+ 
+     // #3
+     albumSongList.innerHTML = '';
+ 
+     // #4
+     for (var i = 0; i < album.songs.length; i++) {
+         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
+     }
+ };
+ 
+ window.onload = function() {
+     setCurrentAlbum(albumPicasso);
+ };
+
+
+var albums = [albumPicasso, albumMarconi, albumMaroon5];
+var i = 1;
+albumImage.addEventListener('click', () => {
+    setCurrentAlbum(albums[i]);
+    i++;
+    if(i == albums.length){
+        i=0;
+    }
+});
 
 
 
